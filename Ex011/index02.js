823d623c65d58573b49a3d2403f90396791e0364
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-08 10:59:38 -0300

    atualizei

--- Ex011/index02.js
+++ Ex011/index02.js
@@ -1,7 +1,9 @@
-var hora = 8
+var hora = 7
 console.log(`Agora são exatamente ${hora}horas.`)
 if (hora < 12) {
     console.log('Bom dia!')
-} else {
+} else if (hora <= 18){
     console.log('Boa tarde!')
+} else {
+    console.log('Boa Noite!')
 }
