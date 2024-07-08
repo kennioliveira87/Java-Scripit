4f1d2af28b583badb39395ba07f15fa428179257
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-08 17:00:43 -0300

    finalizei aula12

--- Aula12/Ex014/script.js
+++ Aula12/Ex014/script.js
@@ -3,15 +3,18 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} Horas:`
     if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+         //BOM DIA!
         img.src = 'imagens/manha250p.png'
+        document.body.style.background = '#755f57'
     } else if (hora >= 12 && hora <= 18) {
         // BOA TARDE!
         img.src = 'imagens/tarde250p.png'
+        document.body.style.background = '#bf8466'
     } else {
         // BOA NOITE!
         img.src = 'imagens/noite250p.png'
+        document.body.style.background = '#182b39'
     }
 }
