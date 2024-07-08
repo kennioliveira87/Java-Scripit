1c9dbe83b1a633949f377d859e44d882e07f061f
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-08 16:22:07 -0300

    atualizei aula012

--- Aula12/Ex014/script.js
+++ Aula12/Ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá Mundo!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/manha250p.png'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'imagens/tarde250p.png'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/noite250p.png'
+    }
+}
