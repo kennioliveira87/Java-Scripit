47ad83f610677c14aaa25b83f8eb9fab662cdacb
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-08 10:52:51 -0300

    atualizei ex011

--- Ex011/index.js
+++ Ex011/index.js
@@ -1,6 +1,8 @@
-var idade = 17
-if (idade < 18) {
-    console.log('Menor de idade.')
+var idade = 66
+if (idade < 16) {
+    console.log('Não Vota.')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto Opcional')
 } else {
-    console.log('Maior de idade.')
+    console.log('Voto obrigatório')
 }
---
+++ index02.js
@@ -0,0 +1,7 @@
+var hora = 8
+console.log(`Agora são exatamente ${hora}horas.`)
+if (hora < 12) {
+    console.log('Bom dia!')
+} else {
+    console.log('Boa tarde!')
+}
