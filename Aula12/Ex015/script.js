bf59d1414856b519856fe78dedeb8b8b0174ba60
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-09 14:20:27 -0300

    criei ex015

---
+++ Aula12/Ex015/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getHours()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        
+    }
+        
+}
