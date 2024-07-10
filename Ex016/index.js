7ec709c3a667f9f40571910363ae08d6f187d30f
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-09 23:14:15 -0300

    criei ex016

---
+++ Ex016/index.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`Olá Mundo! ${c}`)
+    c++
+} while (c <= 10)
+
+
+/*
+
+var c = 1
+while (c <= 10) {
+    console.log(`Olá Mundo! ${c}`)
+    c++
+}
+
+*/
+
+/*
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+console.log('Olá Mundo!')
+*/
+
+
