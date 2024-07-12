a25e62f910796d382fde6f4ad73e4026f1eb4bde
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-11 21:32:38 -0300

    criei ex018

---
+++ Ex018/Modelo/script.js
@@ -0,0 +1,32 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+            res,innerHTML = 'Impossivel contar!'
+            //window.alert('[ERRO] Faltam Dados!')
+    } else {
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }           
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3c1}	`
+    }
+}
