d99937732079d71ab310a2751e70cd1b03ce58e3
Kenni Oliveira <kennioliveira2@outlook.com>
2024-07-12 18:58:11 -0300

    atualizei

--- Ex022/Modelo/script.js
+++ Ex022/Modelo/script.js
@@ -1,1 +1,28 @@
-alert('Olá Mundo!')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, 1) {
+    if (1.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window,alert('tudo ok!')
+    } else {
+        window.alert('Valor invalido ou já encontrado na lista.')
+    }
+}
